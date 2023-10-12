8cee99b5e57acee5422eb423dd40ba8085177090
sseker665 <sseker665@gmail.com>
2023-10-12 14:29:15 +0300

    adding password-manager-project

---
+++ password-manager-project.py
@@ -0,0 +1,31 @@
+master_pwd = input("Enter the master's password: ").lower()
+
+def add():
+    name = input('Account name: ')
+    password = input('Password: ')
+
+    with open ('password.txt','a') as f:
+        f.write(name + '|' + password + '\n')
+
+def view():
+    with open ('password.txt','r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            acc,passw = data.split("|")
+            print("Account: ",acc," Password: ",passw)
+
+
+
+while True:
+    mode = input("Would you like to add new password or view existing ones (add/view), write q to quit program: ").lower()
+
+    if mode == 'q':
+        quit()
+
+    if mode == 'add':
+        add()
+    elif mode == 'view':
+        view()
+    else:
+        print('Invalid mode.')
+        continue 
